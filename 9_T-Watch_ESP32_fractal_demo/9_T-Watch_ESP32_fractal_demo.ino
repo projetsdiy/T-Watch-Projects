8c2650c626071ecfb500011c77ac8bc3debaf6de
Projets DIY <contacthmi@gmail.com>
2020-11-23 17:33:17 +0100

    Add fractal on ESP32 demo
    How to draw Mandelbrot or Julia fractals on your T-Watch. Original source code form FabriceA6

--- 9_T-Watch_ESP32_fractal_demo/9_T-Watch_ESP32_fractal_demo.ino
+++ 9_T-Watch_ESP32_fractal_demo/9_T-Watch_ESP32_fractal_demo.ino
@@ -1,22 +1,22 @@
-/* Switch between LVGL and TFT_eSPI screen on a TTGO T-Watch
- * Landing page is created with LVGL library
- * Arduino IDE or PlatformIO
+/* How to draw Mandelbrot or Julia fractals on your T-Watch. 
+ * Original source code from FabriceA6 https://www.instructables.com/Mandelbrot-and-Julia-Sets-on-ESP32/
  * 
  * Step by step tutorials
+ * 
+ *
  * Licence : see licence file
  * /
  
-/* uncomment your watch */
+/* un-comment your TTGO watch| dé-commenter votre montre TTGO */
 #define LILYGO_WATCH_2019_WITH_TOUCH
 //#define  LILYGO_WATCH_2019_NO_TOUCH
 //#define LILYGO_WATCH_BLOCK
 //#define LILYGO_WATCH_2020_V1
 
-// Arduino IDE - uncomment to activate LVGL library
+/* Activate LVGL library */
 #define LILYGO_WATCH_LVGL
 #include <Arduino.h>
 #include <LilyGoWatch.h>
-
 
 /**************************/
 /*    Static variables    */
@@ -24,26 +24,24 @@
 TTGOClass *ttgo = nullptr;
 TFT_eSPI *tft = nullptr;
 
-
 bool irq = false;
 bool KeyPressed = false;
 int mSelect = 0;
 
-
+/* Display settings | Réglages de l'écran */
 const int TFT_Width  = 240;
 const int TFT_Height = 240;
 const int TFT_MINX   =   0;
 const int TFT_MAXX   = 240;  
 const int TFT_MINY   =   0;
 const int TFT_MAXY   = TFT_Height;
-int boutonY;
+
+//boolean wastouched = true;
+
+// Fractal variables
 #define nb_colors 6
 enum colors {RED, GREEN, BLUE, GRAY, PAL1, PAL2};
 byte Palette = 1;
-// Touch
-boolean wastouched = true;
-
-// Paramètres Mandel
 const float Mandel_MINX = -2.3;
 const float Mandel_MAXX =  0.75;
 const float Mandel_MINY = -1.2;
@@ -61,7 +59,7 @@ float Jcy = 0.01;
 int itermin = 100;
 int itermax = 0;
 boolean Mandel = true;
-byte Puissance = 2;
+byte _power = 2;
 #define Num_puiss 6
 #define Max_iter 64
 #define Escape 100
@@ -73,43 +71,57 @@ void IRAM_ATTR isr()
   touched = true;
 }
 
-
-/* STATIC PROTOTYPE */
-void Draw_Mandel();
-int Calcul_Mandel(float cx, float cy);
-uint32_t Couleur(int i, int mini, int maxi);
-void VisuZone(int i, int j); 
-void CalculZone(int xtft, int ytft);
+/***********************************************************************/
+/*                        STATIC PROTOTYPE                             */
+/***********************************************************************/
+void drawMandel();
+int calculateMandel(float cx, float cy);
+uint32_t getColor(int i, int mini, int maxi);
+void incrementColor();
+void previousZoom();
+void showArea(int i, int j); 
+void calculateArea(int x, int y);
 void resetParameters();
 void buttonClicked();
-void buttonLongClick();
 void buttonDoubleClick();
 void coeffJulia();
 uint8_t menu(void);
 static void menu_event_handler(lv_obj_t * obj, lv_event_t event);
-void incrementColor();
-void previousZoom();
-
-void Draw_Mandel() {
-  unsigned long temps = millis();
+/***********************************************************************/
+/* User button supervisors (not available on T-Watch 2020)             */
+/* Superviseurs bouton utilisateur (non disponible sur T-Watch 2020)   */
+/***********************************************************************/
+void buttonClicked(){
+  Serial.println("Change color");
+  incrementColor();
+  drawMandel();
+}
+void buttonDoubleClick(){
+  Serial.println("Previous zoom");
+  previousZoom();
+  drawMandel();
+}
+/***********************************************************************/
+/*                         Fractals Helpers                            */
+/***********************************************************************/
+void drawMandel() {
+  unsigned long duration = millis();
   float deltaX = (Current_MAXX - Current_MINX) / (TFT_MAXX - TFT_MINX - 1);
   float deltaY = (Current_MAXY - Current_MINY) / (TFT_MAXY - TFT_MINY - 1);
   for (int i = TFT_MINX; i < TFT_MAXX; i++) { // Premier tracé à blanc
     float cx = Current_MINX + i * deltaX;
     for (int j = TFT_MINY; j < TFT_MAXY; j++) {
       float cy = Current_MINY + j * deltaY;
-      int iteration = Calcul_Mandel (cx, cy);
+      int iteration = calculateMandel (cx, cy);
       if (iteration < itermin && iteration > 0) itermin = iteration;
       if (iteration > itermax) itermax = iteration;
-      uint32_t color = Couleur(iteration, 0, Max_iter);
+      uint32_t color = getColor(iteration, 0, Max_iter);
       tft->drawPixel(i, j, color);
     }
   }
-  temps = millis() - temps;
+  duration = millis() - duration;
 
-  Serial.print ("Temps de calcul : ");
-  Serial.print(temps);
-  Serial.println(" ms.");
+  Serial.printf("Rendering time %u ms", duration);
 }
 
 void coeffJulia(){
@@ -117,7 +129,8 @@ void coeffJulia(){
   tft->fillRect(0,  0, TFT_MAXX, TFT_MAXY, TFT_WHITE);
   tft->setTextSize(1);
   tft->setTextColor(TFT_BLACK);
-  tft->drawString("Choisir le coefficient C", 75, 30);
+  // Toucher l'écran pour définir le coefficient C
+  tft->drawString("Touch screen to define C", 75, 30);
   for (int i = 0; i < 4; i++) {
     tft->drawFastVLine(i * TFT_MAXX / 4, 0, TFT_MAXY, TFT_BLACK);
     tft->drawFastHLine(0, i * TFT_MAXY / 4, TFT_MAXX, TFT_BLACK);
@@ -139,10 +152,11 @@ void coeffJulia(){
   float coefY = 2.0 / (TFT_MAXY - TFT_MINY);
   Jcx = -2.0 + xtft * coefX;
   Jcy =  1.0 - ytft * coefY;
+  // Display C coefficient | Affiche le coefficient C
   tft->fillCircle(xtft, ytft, 3, TFT_BLUE);
   sprintf (texte, "Coefficient : ( %f , %f )", Jcx, Jcy);
   tft->drawString(texte, 25, 50);
-  delay(3000);
+  delay(2500);
   float dx = 0.7;
   Current_MINX = Mandel_MINX + dx;
   Current_MINY = Mandel_MINY;
@@ -150,7 +164,8 @@ void coeffJulia(){
   Current_MAXY = Mandel_MAXY;
 }
 
-int Calcul_Mandel(float cx, float cy) {
+// Calculate Mandelbrot / Julia fractal | Calcul la fractal
+int calculateMandel(float cx, float cy) {
   float x1 = 0;
   float y1 = 0;
   float x2 = 0;
@@ -169,7 +184,7 @@ int Calcul_Mandel(float cx, float cy) {
   while (true) {
     if (iteration >= Max_iter) return Max_iter;
     if (x2 * x2 + y2 * y2 > Escape) return iteration;
-    for (int i = 0; i < Puissance - 1; i++) { // Calcul z**n
+    for (int i = 0; i < _power - 1; i++) { // Calculate z**n
       temp = x1 * x2 - y1 * y2;
       y2 = x1 * y2 + x2 * y1;
       x2 = temp;
@@ -179,10 +194,11 @@ int Calcul_Mandel(float cx, float cy) {
     x2 = x1;
     y2 = y1;
     ++iteration;
-  } // Fin Mandelbrot
+  } 
 }
 
-uint32_t Couleur(int i, int mini, int maxi) {
+// Return color | renvoie la couleur
+uint32_t getColor(int i, int mini, int maxi) {
   byte red = 0;
   byte green = 0;
   byte blue = 0;
@@ -229,21 +245,15 @@ uint32_t Couleur(int i, int mini, int maxi) {
   }
   return red << 11 | green << 5 | blue; //tft.color565(red, green, blue);
 }
-
-void VisuZone(int i, int j) {
+void incrementColor(){
+  Palette = (Palette + 1) % nb_colors;
+}
+void showArea(int i, int j) {
   tft->drawCircle(i, j, 4, TFT_WHITE);
   tft->drawRect(i - (TFT_MAXX - TFT_MINX) / 8, j - (TFT_MAXY - TFT_MINY) / 8,
                (TFT_MAXX - TFT_MINX) / 4, (TFT_MAXY - TFT_MINY) / 4, TFT_WHITE);
 }
-
-void resetParameters(){
-  Current_MINX = Mandel_MINX;
-  Current_MINY = Mandel_MINY;
-  Current_MAXX = Mandel_MAXX;
-  Current_MAXY = Mandel_MAXY;
-}
-
-void CalculZone(int xtft, int ytft) {
+void calculateArea(int xtft, int ytft) {
   float coefX = (Current_MAXX - Current_MINX) / (TFT_MAXX - TFT_MINX);
   float Mandel_centerX = Current_MINX + (xtft - TFT_MINX) * coefX;
   float coefY = (Current_MAXY - Current_MINY) / (TFT_MAXY - TFT_MINY);
@@ -259,52 +269,6 @@ void CalculZone(int xtft, int ytft) {
   Current_MINY = Mandel_centerY - DeltaY;
   Current_MAXY = Mandel_centerY + DeltaY;
 }
-
-void setup() {
-  Serial.begin(115200);
-  // Get ttgp object and set up the display | Récupère l'objet et allume l'écran
-  ttgo = TTGOClass::getWatch();
-  ttgo->begin();     
-  ttgo->openBL();
-  ttgo->lvgl_begin();
-  // Easy access to TFT library functions | Accès facile aux fonctions de la librairie TFT 
-  tft = ttgo->tft;
-  tft->fillScreen(TFT_BLACK);
-
-  Draw_Mandel();
-
-  //pinMode(36, INPUT_PULLUP);
-  //attachInterrupt(36, isr, FALLING);
-
-  ttgo->button->setLongClickHandler(buttonLongClick);
-  ttgo->button->setClickHandler(buttonClicked);  
-  ttgo->button->setDoubleClickHandler(buttonDoubleClick);
-
-  pinMode(AXP202_INT, INPUT_PULLUP);
-  attachInterrupt(AXP202_INT, [] {
-      irq = true;
-  }, FALLING);
-  //!Clear IRQ unprocessed  first
-  ttgo->power->enableIRQ(AXP202_PEK_SHORTPRESS_IRQ | AXP202_VBUS_REMOVED_IRQ | AXP202_VBUS_CONNECT_IRQ | AXP202_CHARGING_IRQ, true);
-  ttgo->power->clearIRQ();
-}
-
-void buttonClicked(){
-  Serial.println("Change color");
-  incrementColor();
-  Draw_Mandel();
-  
-}
-void buttonDoubleClick(){
-  previousZoom();
-  Draw_Mandel();
-  Serial.println("Previous zoom");
-}
-
-void buttonLongClick(){
-  Serial.println("Long click...");
-}
-
 void previousZoom(){
   Serial.println ("Previous");
   Current_MINX = Prev_MINX;
@@ -312,9 +276,43 @@ void previousZoom(){
   Current_MAXX = Prev_MAXX;
   Current_MAXY = Prev_MAXY;
 }
+void resetParameters(){
+  _power = 2;
+  Palette = 1;
+  Current_MINX = Mandel_MINX;
+  Current_MINY = Mandel_MINY;
+  Current_MAXX = Mandel_MAXX;
+  Current_MAXY = Mandel_MAXY;
+}
+/***********************************************************************/
+/*                          Setup & loop                               */
+/***********************************************************************/
+void setup() {
+  Serial.begin(115200);
+  // Get ttgp object and set up the display | Récupère l'objet et allume l'écran
+  ttgo = TTGOClass::getWatch();
+  ttgo->begin();     
+  ttgo->openBL();
+  ttgo->lvgl_begin();
+  // Easy access to the functions of the TFT library 
+  // Accès facile aux fonctions de la librairie TFT 
+  tft = ttgo->tft;
+  tft->fillScreen(TFT_BLACK);
 
-void incrementColor(){
-  Palette = (Palette + 1) % nb_colors;
+  drawMandel();
+
+  // User button handler | Superviseur bouton utilisateur
+  ttgo->button->setClickHandler(buttonClicked);  
+  ttgo->button->setDoubleClickHandler(buttonDoubleClick);
+
+  // Attaches the AXP202 interrupt to detect user action on the home button 
+  // Attache l'interruption AXP202 pour détecter l'action de l'utilisateur sur le bouton principal
+  pinMode(AXP202_INT, INPUT_PULLUP);
+  attachInterrupt(AXP202_INT, [] {
+      irq = true;
+  }, FALLING);
+  ttgo->power->enableIRQ(AXP202_PEK_SHORTPRESS_IRQ | AXP202_VBUS_REMOVED_IRQ | AXP202_VBUS_CONNECT_IRQ | AXP202_CHARGING_IRQ, true);
+  ttgo->power->clearIRQ();
 }
 
 void loop() {
@@ -326,7 +324,7 @@ void loop() {
       if ( ttgo->power->isPEKLongtPressIRQ() ) {
           Serial.println("Power button long press -> Reset Parameters");
           resetParameters();
-          Draw_Mandel();
+          drawMandel();
       }
       if (ttgo->power->isPEKShortPressIRQ()) {
         Serial.println("Power button pressed -> Display Menu");
@@ -351,13 +349,13 @@ void loop() {
             Current_MAXX = Mandel_MAXX + dx;
             Current_MAXY = Mandel_MAXY; 
             break; 
-          case 4:  // Coefficient Julia
+          case 4:  // Open Julia Coefficient page
             coeffJulia();
             break;
-          case 5:
-            // Puissance
-            Puissance = 2 + (Puissance - 1) % Num_puiss;
-            if (Puissance != 2) dx = 0.65;
+          case 5: // power | Puissance
+            _power = 2 + ( _power - 1 ) % Num_puiss;
+            Serial.printf("power %.1f", _power);
+            if ( _power != 2 ) dx = 0.65;
             Current_MINX = Mandel_MINX + dx;
             Current_MINY = Mandel_MINY;
             Current_MAXX = Mandel_MAXX + dx;
@@ -366,18 +364,10 @@ void loop() {
           default:
             break;  
         }
-        Draw_Mandel();
-          //ttgo->tft->fillRect(20, 100, 200, 85, TFT_BLACK);
-          //ttgo->tft->drawString("PowerKey Press", 25, 100);
+        drawMandel();
       }
       ttgo->power->clearIRQ();
   }
-
-  if ( touched ) {
-    Serial.println("Button :)");
-    touched = false;
-  }
-
   if ( ttgo->getTouch(x, y) ) {
     int startup = millis();
     while ( ttgo->getTouch(x, y) ) {}
@@ -385,30 +375,30 @@ void loop() {
       Serial.println("Long touch -> Reset Fractal");
       
     } else {
-      VisuZone(x, y);
-      CalculZone(x, y);
-      Draw_Mandel();
+      showArea(x, y);
+      calculateArea(x, y);
+      drawMandel();
     }
   }
+  // Run user button handler | Appel le superviseur du bouton utilisateur
+  // No available on T-Watch 2020 | Non disponible sur T-Watch 2020
   ttgo->button->loop();
 }
-
-uint8_t menu(void)
-{
-    const char* typeFractal = "MANDEL";
-    if ( !Mandel ) typeFractal = "JULIA";
+/***********************************************************************/
+/*                              Menu                                   */
+/***********************************************************************/
+uint8_t menu(void) {
     static const char * btnm_map[] = 
       {"RESET", "\n",
        "UNDO", "\n",
        "COLOR", "\n",
-       typeFractal, "Coeff. Julia", "\n",
-       "PARAM", ""};
-                                  
+       "M/J", "Julia Coeff. ", "\n",
+       "POWER", ""};                            
     KeyPressed = false;
     lv_obj_t * btnm1 = lv_btnmatrix_create(lv_scr_act(), NULL);
     
     lv_btnmatrix_set_map(btnm1, btnm_map);
-    lv_btnmatrix_set_btn_width(btnm1, 10, 2);        /*Make "Action1" twice as wide as "Action2"*/
+    lv_btnmatrix_set_btn_width(btnm1, 10, 2);        
     lv_btnmatrix_set_btn_ctrl(btnm1, 10, LV_BTNMATRIX_CTRL_CHECKABLE);
     lv_btnmatrix_set_btn_ctrl(btnm1, 11, LV_BTNMATRIX_CTRL_CHECK_STATE);
     lv_obj_set_size(btnm1, 240, 240);
@@ -416,7 +406,6 @@ uint8_t menu(void)
     lv_obj_set_event_cb(btnm1, menu_event_handler);
     
     while (!KeyPressed) {lv_task_handler(); delay(20);} // Wait for touch
-    //ttgo->tft->fillScreen(TFT_BLACK);
     return mSelect;
 }
 
